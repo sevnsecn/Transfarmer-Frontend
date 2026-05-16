83dcd337d6918044d73d18afd43ab75903e1b45f
sevnsecn <arthurmatthewpg@gmail.com>
2026-05-17 01:09:42 +0700

    dockerfile and docker ignore, docker compose is located on backend repo

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5000"; //forwards all /api/* calls to the backend
+const AUTH_SERVICE_URL =
+  process.env.AUTH_SERVICE_URL || "http://localhost:5001";
+
+const FARM_SERVICE_URL =
+  process.env.FARM_SERVICE_URL || "http://localhost:5002";
+
+const PRODUCT_SERVICE_URL =
+  process.env.PRODUCT_SERVICE_URL || "http://localhost:5003";
+
+const ORDER_SERVICE_URL =
+  process.env.ORDER_SERVICE_URL || "http://localhost:5004";
+
+const UPLOAD_SERVICE_URL =
+  process.env.UPLOAD_SERVICE_URL || "http://localhost:5005";
 
 const nextConfig: NextConfig = {
   images: {
@@ -11,7 +24,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "**.cloudinary.com", // Catch all Cloudinary subdomains
+        hostname: "**.cloudinary.com",
       },
       {
         protocol: "http",
@@ -19,17 +32,39 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
+//the ..._SERVICE_URL are parameters used to set the URL of the backend services depending on how we run it (local or docker or even production)
+  async rewrites() { 
     return [
-      { source: "/api/auth/:path*", destination: "http://localhost:5001/api/auth/:path*" },
-      { source: "/api/users/:path*", destination: "http://localhost:5001/api/users/:path*" },
-      { source: "/api/farms/:path*", destination: "http://localhost:5002/api/farms/:path*" },
-      { source: "/api/products/:path*", destination: "http://localhost:5003/api/products/:path*" },
-      { source: "/api/orders/:path*", destination: "http://localhost:5004/api/orders/:path*" },
-      { source: "/api/orderItems/:path*", destination: "http://localhost:5004/api/orderItems/:path*" },
-      { source: "/api/upload/:path*", destination: "http://localhost:5005/api/upload/:path*" },
-    ]
-  }
+      {
+        source: "/api/auth/:path*",
+        destination: `${AUTH_SERVICE_URL}/api/auth/:path*`,
+      },
+      {
+        source: "/api/users/:path*",
+        destination: `${AUTH_SERVICE_URL}/api/users/:path*`,
+      },
+      {
+        source: "/api/farms/:path*",
+        destination: `${FARM_SERVICE_URL}/api/farms/:path*`,
+      },
+      {
+        source: "/api/products/:path*",
+        destination: `${PRODUCT_SERVICE_URL}/api/products/:path*`,
+      },
+      {
+        source: "/api/orders/:path*",
+        destination: `${ORDER_SERVICE_URL}/api/orders/:path*`,
+      },
+      {
+        source: "/api/orderItems/:path*",
+        destination: `${ORDER_SERVICE_URL}/api/orderItems/:path*`,
+      },
+      {
+        source: "/api/upload/:path*",
+        destination: `${UPLOAD_SERVICE_URL}/api/upload/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
